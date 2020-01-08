884fe760ee0fec7cf7f85c422e9c683b3b5fcbc6
ANDREW-PC\Andrew Richards <arich72491@gmail.com>
2020-01-08 14:49:22 -0600

    Defined portfolio constructor, calcReturns, and printReturns.

--- Main.py
+++ Main.py
@@ -5,11 +5,11 @@ Created on Wed Jan  8 12:47:43 2020
 @author: Andrew Richards
 """
 
-import Portfolio
-
+from Portfolio import portfolio
 
 
 tickers = ['WIKI/MSFT', 'WIKI/T', 'WIKI/F', 'WIKI/GE']
 weights = [0.25, 0.25, 0.25, 0.25]
-portfolio1 = Portfolio.portfolio(tickers, weights)
 
+portfolio1 = portfolio(tickers, weights)
+portfolio1.printReturns()
--- Portfolio.py
+++ Portfolio.py
@@ -7,30 +7,36 @@ Created on Wed Jan  8 12:48:21 2020
 
 import numpy as np
 import pandas as pd
-import datetime
+from datetime import datetime
 import quandl
 quandl.ApiConfig.api_key = ""
 
 class portfolio:
     
-    assets = []
+    tickers = []
     weights = []
+    start = datetime(1995, 1, 1)
+    #end = datetime.datetime(2019, 1, 27)
+    rawData = pd.DataFrame()
     
     def __init__(self, tickers, weights):
-        self.assets = tickers
-        self.weights = weights
-        
-    def initData():
-        return 0
-
-    def calcReturn():
-        return 0
+        self.tickers = tickers
+        self.weights = np.array(weights)
+        for t in self.tickers:
+            self.rawData[t] = quandl.get(t, start_date=self.start)['Adj. Close']
+            
+    
+    
+    def calcReturn(self):
+        simpleReturns = (self.rawData / self.rawData.shift(1)) - 1
+        annualReturns = simpleReturns.mean() * 250 
+        return np.dot(annualReturns, self.weights)
         
     def calcRisk():
         return 0
     
-    def printReturn():
-        return 0
+    def printReturns(self):
+        print('Annual Portfolio Return: ' + str(round(self.calcReturn()*100, 3)) + '%')
     
     def printRisk():
         return 0
